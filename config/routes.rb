084f8b33845271f6a5c1cc38504f9fed54fbc0bd
hirokazu.matsuoka <gha716083@lib.bbiq.jp>
2019-11-10 06:21:09 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
+  get 'users/edit'
+  get 'users/update'
+  get 'users/destroy'
   root to: 'tasks#index'
-    
-  resources :tasks
+  
+  get 'signup', to: 'users#new'  
+  resources :users, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 end
