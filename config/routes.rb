78ebbd37b6b05bd98536c2a4b53fda2ba4d0c25d
hirokazu.matsuoka <gha716083@lib.bbiq.jp>
2019-11-11 12:33:10 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
-  get 'users/edit'
-  get 'users/update'
-  get 'users/destroy'
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
   root to: 'tasks#index'
   
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  
   get 'signup', to: 'users#new'  
-  resources :users, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  resources :users, only: [:new, :create]
+  resources :tasks
 end
